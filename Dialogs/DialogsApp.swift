ce13b58f9f85aa90e7368d30c5c6c23e61146a15
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-10-28 15:40:48 +0600

    project init

---
+++ Dialogs/DialogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DialogsApp.swift
+//  Dialogs
+//
+//  Created by Shah Md Imran Hossain on 28/10/23.
+//
+
+import SwiftUI
+
+@main
+struct DialogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
