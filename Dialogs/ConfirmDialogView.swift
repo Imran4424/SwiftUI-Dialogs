e44baa30bc1909a26019fd7220aed8db1850c581
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-10-28 15:47:44 +0600

    refactor to confirm dialog

--- Dialogs/ConfirmDialogView.swift
+++ Dialogs/ConfirmDialogView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  ConfirmDialogView.swift
 //  Dialogs
 //
 //  Created by Shah Md Imran Hossain on 28/10/23.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ConfirmDialogView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    ConfirmDialogView()
 }
