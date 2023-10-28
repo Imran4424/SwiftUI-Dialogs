258cf40036ed032a78210d661f01b767764a1ed7
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-10-28 15:48:29 +0600

    complete confirm diaglog

--- Dialogs/ConfirmDialogView.swift
+++ Dialogs/ConfirmDialogView.swift
@@ -8,16 +8,42 @@
 import SwiftUI
 
 struct ConfirmDialogView: View {
+    @State private var showingConfirmation = false
+    @State private var backgroundColor = Color.white
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
-                .foregroundStyle(.tint)
+                .foregroundStyle(.black)
             Text("Hello, world!")
         }
-        .padding()
+        .frame(width: 300, height: 300)
+        .background(backgroundColor)
+        .onTapGesture {
+            showingConfirmation = true
+        }
+        .confirmationDialog("Change background", isPresented: $showingConfirmation) {
+            Button("Red") {
+                backgroundColor = .red
+            }
+            
+            Button("Green") {
+                backgroundColor = .green
+            }
+            
+            Button("Blue") {
+                backgroundColor = .blue
+            }
+            
+            Button("Cancel", role: .cancel) { }
+        } message: {
+            Text("Select a new color")
+        }
+
     }
 }
+
 
 #Preview {
     ConfirmDialogView()
